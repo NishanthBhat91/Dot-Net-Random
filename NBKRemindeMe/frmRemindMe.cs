fac94f05bda17efdecced45def4163cf4a59ea26
NishanthBhat <nishant.sukrutha@gmail.com>
2024-09-20 09:24:23 +0530

    Initial commit

---
+++ NBKRemindeMe/frmRemindMe.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Linq;
+using System.Resources;
+using Timer = System.Windows.Forms.Timer;
+
+namespace NBKRemindeMe
+{
+    public partial class frmRemindMe : Form
+    {
+        public frmRemindMe()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRemindMe_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
